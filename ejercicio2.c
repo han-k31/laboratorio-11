83f3d9d695fad280e9f86bc91da17f62357958e5
han-k31 <hannasgiron@gmail.com>
2025-11-13 13:46:34 -0600

    Update ejercicio2.c

--- ejercicio2.c
+++ ejercicio2.c
@@ -4,14 +4,31 @@ laboratorio 11 ejercicio 2*/
 #include <stdio.h> 
 
 void sumar(); // prototipo de la función  
+int suma2(int a, int b); //prototipado de funcion con parametros y regreso
+
 int main() 
 { 
-  sumar(); // llamado de la función suma 
+    int a, b, res;
+    
+    printf("Dame el primer valor: ");
+    scanf("%d", &a);
+    printf("Dane el segundo valor: ");
+    scanf("%d", &b);
+    sumar(); // llamado de la función suma 
+    res = suma2(a, b);
+    
+    printf("El resultado de la suma es %d\n", res);
+    return 0;
 } 
 
 void sumar() // función suma 
 { 
-  int z, x=5, y=10; //variables locales 
-  z=x+y; 
-  printf("%i",z); 
+    int z, x=5, y=10; //variables locales 
+    z=x+y; 
+    printf("El resultado es: %i",z); 
+}
+
+int suma2(int a, int b)
+{
+    return a + b;
 }
