f6dfea8a5cad9f66fbca698f3db036319246d688
han-k31 <hannasgiron@gmail.com>
2025-11-13 14:18:12 -0600

    Create ejercicio6.c

---
+++ ejercicio6.c
@@ -0,0 +1,22 @@
+/*Ggirón Knepp Hanna Sofía
+laboratorio 11 Ejercicio 6 */
+
+#include <stdio.h> 
+
+void llamarFuncion(); 
+
+int main() 
+{ 
+  for (int j=0 ; j < 5 ; j++) 
+  { 
+    llamarFuncion(); 
+  } 
+} 
+void llamarFuncion() 
+{ 
+  /* Solo la primera vez que se llame a esta función se creará y se le asignará  
+  el valor de 0 a la variable estática numVeces */ 
+  static int numVeces = 0; //variable estatica
+  numVeces = numVeces + 1;
+  printf("Esta función se ha llamado %d veces.\n",++numVeces); 
+}
