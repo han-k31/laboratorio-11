73d5f0a83cf64b85894c1d4320a3cc73d0e9de00
han-k31 <hannasgiron@gmail.com>
2025-11-21 11:24:10 -0600

    Update ejercicio2.c

--- ejercicio2.c
+++ ejercicio2.c
@@ -1,34 +1,39 @@
 /* Girón Knepp Hanna Sofía
 laboratorio 11 ejercicio 2*/
 
-#include <stdio.h> 
+/*Alfonso Francisco De Abiega L'Eglisse
+laboratorio 11 Programa 2*/
 
-void sumar(); // prototipo de la función  
-int suma2(int a, int b); //prototipado de funcion con parametros y regreso
+#include <stdio.h>
 
-int main() 
-{ 
-    int a, b, res;
-    
-    printf("Dame el primer valor: ");
-    scanf("%d", &a);
-    printf("Dane el segundo valor: ");
-    scanf("%d", &b);
-    sumar(); // llamado de la función suma 
-    res = suma2(a, b);
-    
-    printf("El resultado de la suma es %d\n", res);
-    return 0;
-} 
+void sumar(); // prototipo de la función
+int suma2(int a, int b); //prototipado de función con parametros y regreso
 
-void sumar() // función suma 
-{ 
-    int z, x=5, y=10; //variables locales 
-    z=x+y; 
-    printf("El resultado es: %i",z); 
+int main()
+{
+  int a, b, res;
+  
+  printf("Dame el primer valor:  ");
+  scanf("%d", &a);
+  printf("Dame el segundo valor:  ");
+  scanf("%d", &b);
+  
+  sumar(); // llamado de la función suma
+  res = suma2(a, b);
+  
+  printf("El resultado de la suma es %d\n", res);
+  return 0;
 }
 
-int suma2(int a, int b)
+void sumar() //función suma
 {
-    return a + b;
+  int z, x=6, y=10; //variables locales
+  z=x+y;
+  printf("El resultado es:  %i\n",z);
+}
+
+int suma2(int a, int b) //función que regresa el resultado de la suma
+{
+    
+    return a + b; //regresa resultado
 }
