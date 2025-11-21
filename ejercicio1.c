12dc8403dc9f36223cd562977790b017055d0d02
han-k31 <hannasgiron@gmail.com>
2025-11-21 11:33:17 -0600

    Update ejercicio1.c

--- ejercicio1.c
+++ ejercicio1.c
@@ -1,32 +1,37 @@
 /* Girón Knepp Hanna Sofía
-laboratorio 11 ejercicio 11*/
+laboratorio 11 ejercicio 1*/
 
-#include <stdio.h> 
-#include <string.h> 
+#include <stdio.h>
+#include <string.h>
 
-// Prototipo o firma de las funciones del programa 
-void imprimir(char[]); 
-// Definición o implementación de la función main 
+// Prototipo o firma de las funciones del programa
+void imprimir(char[]);
+void imprimir2(char[]);
 
+// Definición o implementación de la función main
 int main()
-{ 
-  char nombre[] = "Facultad de Ingeniería"; 
+{
+  char nombre[] = "Facultad de Ingenieria";
   imprimir(nombre);
+  imprimir2(nombre);
   return 0;
-} 
+}
 
-// Implementación de las funciones del programa 
-void imprimir(char s[]){ 
-  int tam; 
-  for ( tam=strlen(s)-1 ; tam>=0 ; tam--) 
-    printf("%c", s[tam]); 
-  printf("\n"); 
-} 
-// Implementación de las funciones del programa 
-void imprimir2(char s[]){ 
+// Implementación de las funciones del programa
+void imprimir(char s[])
+{
+  int tam;
+  for ( tam=strlen(s)-1 ; tam>=0 ; tam--)
+    printf("%c", s[tam]);
+  printf("\n");
+}
+
+void imprimir2(char s[])
+{
   int tam, i;
-  tam = strlen(s);
-  for (i = 0; i < tam; i++) 
-    printf("%c", s[i]); 
-  printf("\n"); 
-} 
+  tam = strlen(s) - 1;
+  
+  for (i = 0;  i <= tam; i++)
+    printf("%c", s[i]);
+  printf("\n");
+}
